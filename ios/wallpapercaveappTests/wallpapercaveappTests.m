0cc3c5d274e391e9a59c5723ae99d8e9a4d43eb7
akmal.elahi <akmal.elahi@m3tech.com.pk>
2020-08-24 16:33:35 +0500

    feat:change app name

--- ios/wallpapercaveappTests/wallpapercaveappTests.m
+++ ios/wallpapercaveappTests/wallpapercaveappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface imageGalleryAppTests : XCTestCase
+@interface wallpapercaveappTests : XCTestCase
 
 @end
 
-@implementation imageGalleryAppTests
+@implementation wallpapercaveappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
